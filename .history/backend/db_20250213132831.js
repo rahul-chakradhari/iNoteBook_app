a30b8f44933e5b1dba83191e7771a4c298a01df7
Rahul Chakradhari <96400194+rahul-chakradhari@users.noreply.github.com>
2025-02-14 23:16:41 +0530

    Delete .history directory

--- .history/backend/db_20250213132831.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const mongoURI = "mongodb://localhost:27017/"; // Ensure MongoDB is running
-
-const connectToMongo = async (mongoURI) => {
-  clg("connected to mongo sugc");
-};
-module.exports = connectToMongo;
