12ccdec6c2768ea88c525e1952f1269184efd6f5
rahul-chakradhari <rahulchakradhari10september@gmail.com>
2025-02-18 17:06:20 +0530

    successfull_finally

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,12 @@
-// db.js
 const mongoose = require("mongoose");
+const mongoURI = "mongodb://localhost:27017/notes";
 
 const connectToMongo = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/notes", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to MongoDB");
+    await mongoose.connect(mongoURI);
+    console.log("✅ Connected to MongoDB");
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
+    console.error("❌ Error connecting to MongoDB:", error);
   }
 };
 
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,17 @@
 const connectToMongo = require("./db");
 const express = require("express");
 var cors = require("cors");
-const mongoose = require("mongoose");
 
 connectToMongo();
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000; // Render ke liye dynamic port
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("iNotebook Backend is Running Successfully! 🚀");
+});
 
 // Available Routes
 app.use("/api/auth", require("./routes/auth"));
